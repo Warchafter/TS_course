b4396899d1b16981cb9f96aa477c2908cce8f6cb
Warchafter <kevin.arriaga@gmail.com>
2024-08-27 14:55:57 -0400

    Seperated logic for sorting algorithm

---
+++ S10L78/sort/src/NumbersCollection.ts
@@ -0,0 +1,13 @@
+export class NumbersCollection {
+    // data: number[];
+
+    // constructor(data: numbers[]) {
+    //     this.data = data;
+    // }
+    /* This lines above are the exact same thing as the one below */
+    constructor(public data: number[]) {}
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        
+    }
+}
